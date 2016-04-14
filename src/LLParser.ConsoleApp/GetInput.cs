82b671d022e84b75654049149a96c46d851a0bd7
Cy Scott <cy.a.scott@live.com>
2016-04-14 17:38:15 -0400

    Project Created

---
+++ src/LLParser.ConsoleApp/GetInput.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LLParser.ConsoleApp
+{
+    public static partial class Program
+    {
+        public static void GetInput()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("[Getting Input]");
+            Console.WriteLine();
+
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Input the first grammer followed by the endter key (i.e. E:a or E:a|b)");
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+
+
+        }
+    }
+}
---
+++ src/LLParser.ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LLParser.ConsoleApp
+{
+    public static partial class Program
+    {
+        static Program()
+        {
+            Console.BufferHeight = 1024;
+            Console.BufferWidth = 1024;
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public static void Main()
+        {
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("[LL Parser Console App]");
+                Console.WriteLine();
+
+                GetInput();
+
+
+            }
+            catch (Exception error)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(error);
+            }
+        }
+    }
+}
